5a59d49b75de33cdf9c8f1cfa9f92e845efd2a7e
Kirill Kapustin <kanyct04ka@yandex.ru>
2025-08-29 05:54:45 +0300

    initial inserts changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 MERGE INTO mpa (id, name) KEY(name) VALUES (1, 'G');
-MERGE INTO mpa (id, name) KEY(name) VALUES (2, 'PG');
-MERGE INTO mpa (id, name) KEY(name) VALUES (3, 'PG-13');
-MERGE INTO mpa (id, name) KEY(name) VALUES (4, 'R');
-MERGE INTO mpa (id, name) KEY(name) VALUES (5, 'NC-17');
+MERGE INTO mpa (id, name) KEY(name) VALUES (3, 'PG');
+MERGE INTO mpa (id, name) KEY(name) VALUES (4, 'PG-13');
+MERGE INTO mpa (id, name) KEY(name) VALUES (5, 'R');
+MERGE INTO mpa (id, name) KEY(name) VALUES (2, 'NC-17');
 
-MERGE INTO genres (id, name) KEY(name) VALUES (1, 'Комедия');
-MERGE INTO genres (id, name) KEY(name) VALUES (2, 'Драма');
-MERGE INTO genres (id, name) KEY(name) VALUES (3, 'Мультфильм');
-MERGE INTO genres (id, name) KEY(name) VALUES (4, 'Триллер');
-MERGE INTO genres (id, name) KEY(name) VALUES (5, 'Документальный');
-MERGE INTO genres (id, name) KEY(name) VALUES (6, 'Боевик');
+MERGE INTO genres (id, name) KEY(name) VALUES (4, 'Комедия');
+MERGE INTO genres (id, name) KEY(name) VALUES (3, 'Драма');
+MERGE INTO genres (id, name) KEY(name) VALUES (5, 'Мультфильм');
+MERGE INTO genres (id, name) KEY(name) VALUES (6, 'Триллер');
+MERGE INTO genres (id, name) KEY(name) VALUES (2, 'Документальный');
+MERGE INTO genres (id, name) KEY(name) VALUES (1, 'Боевик');
