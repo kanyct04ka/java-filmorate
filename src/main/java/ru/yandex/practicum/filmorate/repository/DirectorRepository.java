3010d539b090415c3b66f3eba31221f2880950f4
Nohum <savva.gubarev228@mail.ru>
2025-09-18 23:53:43 +0300

    Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/repository/DirectorRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/DirectorRepository.java
@@ -4,7 +4,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Director;
-import ru.yandex.practicum.filmorate.repository.mapper.DirectorRowMapper;
 
 import java.util.List;
 import java.util.Optional;
