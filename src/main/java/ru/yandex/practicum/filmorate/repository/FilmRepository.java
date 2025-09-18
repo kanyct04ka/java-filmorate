088cf576db573dd937918073a88d31dc090bbee4
Nohum <savva.gubarev228@mail.ru>
2025-09-18 13:25:57 +0300

    Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/repository/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/FilmRepository.java
@@ -6,7 +6,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.EntityUpdateErrorException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
