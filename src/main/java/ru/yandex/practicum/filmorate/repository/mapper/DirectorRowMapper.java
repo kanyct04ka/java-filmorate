b23d156b1e217f3f27e27d29fb0ab58d6c182e00
Nohum <savva.gubarev228@mail.ru>
2025-09-19 00:00:18 +0300

    Update DirectorRowMapper.java

--- src/main/java/ru/yandex/practicum/filmorate/repository/mapper/DirectorRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/mapper/DirectorRowMapper.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.repository.mapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Director;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -12,9 +11,9 @@ public class DirectorRowMapper implements RowMapper<Director> {
 
     @Override
     public Director mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return Director.builder()
-                .id(rs.getInt("id"))
-                .name(rs.getString("name"))
-                .build();
+        Director director = new Director();
+        director.setId(rs.getInt("id"));
+        director.setName(rs.getString("name"));
+        return director;
     }
 }
