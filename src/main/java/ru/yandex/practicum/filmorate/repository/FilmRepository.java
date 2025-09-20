8c16a793b040471886243d048782c335cca90550
Nohum <savva.gubarev228@mail.ru>
2025-09-20 17:05:25 +0300

    Update FilmRepository.java

--- src/main/java/ru/yandex/practicum/filmorate/repository/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/FilmRepository.java
@@ -227,7 +227,7 @@ public class FilmRepository extends BaseRepository<Film> {
         if ("year".equalsIgnoreCase(sortBy)) {
             orderBy = "ORDER BY f.release_date ASC";
         } else if (sortBy == null || "likes".equalsIgnoreCase(sortBy)) {
-            orderBy = "ORDER BY like_count DESC, f.release_date ASC";
+            orderBy = "ORDER BY like_count DESC";
         } else {
             throw new IllegalArgumentException("Неверный параметр сортировки: " + sortBy);
         }
